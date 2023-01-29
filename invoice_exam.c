8e3115fa0c4719aec578ee8e113ad3b6863397f4
Sikiteeb <sihann@ttu.ee>
2023-01-29 11:42:25 +0200

    Update invoice_exam.c

--- invoice_exam.c
+++ invoice_exam.c
@@ -1,66 +1,59 @@
-#include "stdio.h"
-#include "stdlib.h"
-#include "ctype.h"
-#include "string.h"
-#include "time.h"
-#pragma warning ( disable : 4996 ) //deprecation warnings off
+int main() {
+    int n;
+    char **PassengerList = Exam(InvoiceList, 7, "Guatemala City", "10-Feb-2022", &n);
+    if (!PassengerList) {
+        printf("No matches found\n");
+    } else {
+        printf("Passengers flying to Guatemala City on 10-Feb-2022:\n");
+        for (int i = 0; i < n; i++) {
+            printf("%s\n", PassengerList[i]);
+        }
+    }
+    return 0;
+}
 
-struct tm timeinfo;
-typedef struct date
-{
-    int day;
+
+
+char** Exam(const INVOICE** ppInvoices, int nInvoices, const char* pDestination, const char *pDate, int *pnResults) {
+    // error checking
+    if (!ppInvoices || !pDestination || !pDate || !pnResults) return 0;
+
+    // extract day, month, and year from pDate
+    int day, year;
     char month[4];
-    int year;
-}DATE;
-typedef struct invoice
-{
-    const char *pPassenger,
-    *pDestination;
-    int fare;
-    DATE when;
-} INVOICE;
+    sscanf(pDate, "%d-%3s-%d", &day, month, &year);
 
-const INVOICE* InvoiceList[]={
-        new INVOICE {"John Smith", "Buenos Aires", 2000, { 1, "Mar", 2022 }},
-        new INVOICE {"Mary Weaver", "Guatemala City", 1700, { 10, "Feb", 2022 }},
-        new INVOICE {"James Carpenter", "Santiago de Chile", 2200, { 1, "Mar", 2022 }},
-        new INVOICE {"Nancy Gardener", "Guatemala City", 1700, { 10, "Mar", 2022 }},
-        new INVOICE {"Elizabeth Clerk", "Quito", 1800, { 12, "Apr", 2022 }},
-        new INVOICE {"Thomas Farmhand", "Guatemala City", 1700, { 11, "Feb", 2022 }},
-        new INVOICE {"Richard Sailor", "Guatemala City", 1700, { 10, "Feb", 2022 }}};
+    // create an array to store the matching passenger names
+    char **results = (char**)malloc(nInvoices * sizeof(char*));
+    int nResults = 0;
 
-
-int main()
-{
-
-     scanf("%19s", InvoiceList[i].pPassenger );
-     scanf("%19s", InvoiceList[i].pDestination );
-     scanf("%19s", InvoiceList[i].Date );
-
-
-     int length = sizeof InvoiceList[]/sizeof InvoiceList[0];
-
-     INVOICE** traveller(const INVOICE** ppInvoices, int nInvoices, const char* pDestination, const char *pDate, int *pnResults);
-        for (int i = 0; i < length; i++) {
-
-        printf("Passenger: %s , Destination:  %s, Date:  %s", InvoiceList[i]->pPassenger,InvoiceList[i]->pDestination, InvoiceList[i]-> Date.day, InvoiceList[i] -> Date.month, InvoiceList[i]-> Date.year );
+    // loop through the invoices and check for matches
+    for (int i = 0; i < nInvoices; i++) {
+        const INVOICE *invoice = ppInvoices[i];
+        if (strcmp(invoice->pDestination, pDestination) == 0 &&
+            invoice->when.day == day &&
+            strcmp(invoice->when.month, month) == 0 &&
+            invoice->when.year == year) {
+                // found a match, add passenger name to results array
+                results[nResults] = (char*)malloc((strlen(invoice->pPassenger) + 1) * sizeof(char));
+                strcpy(results[nResults], invoice->pPassenger);
+                nResults++;
+        }
     }
 
+    // if no matches were found, return 0
+    if (nResults == 0) return 0;
 
-return 0;
+    // return the results array and the number of matches
+    *pnResults = nResults;
+    return results;
 }
+
 //if the call is:
 //int n;
 //char **PassengerList=Exam(InvoiceList,7,"Guatemala City","10-Feb-2022", &n);
 //then the result should include the names, "Mary Weaver" and "Richard Sailor".
 
-char** Exam(const INVOICE** ppInvoices, int nInvoices, const char* pDestination, const char *pDate, int *pnResults)
-{
-
-   if(!ppInvoices || !nInvoices || !pDestination || !pDate || !pnResults)
-   {
-      return 0;
-   }
 
 
 
@@ -69,26 +62,3 @@ char** Exam(const INVOICE** ppInvoices, int nInvoices, const char* pDestination,
 
 
 
-
-       time(&now);
-    localtime_s(&timeinfo, &now);
-    for (int i = 0; i < counter; i++)//find the time
-    {
-        InvoiceList[i]->when.day = timeinfo.tm_mday;//day
-        for (int j = 0; j < 12; j++)//fill in the month array
-        {
-            char months[][4] = { "Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
-            if (timeinfo.tm_mon + 1 == j+1)
-            {
-                for (int o = 0; o < 3; o++)
-                {
-                   InvoiceList[i]->when.month[o] = months[j][o];
-                }
-                in[i]->date.month[3] = '\0';
-            }
-        }
-        InvoiceList[i]->when.year = timeinfo.tm_year + 1900;//year
-    }
-    return InvoiceList;
-
-}
