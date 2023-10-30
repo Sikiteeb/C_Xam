766c393e974c563bcb01c3dc7756dd3686f3d3c8
Sigrid Hanni <sihann@taltech.ee>
2023-10-30 09:55:25 +0200

    Passed

--- invoice_exam.c
+++ invoice_exam.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 
 typedef struct date {
     int day;
@@ -14,62 +14,87 @@ typedef struct invoice {
     DATE when;
 } INVOICE;
 
-const INVOICE* InvoiceList[]={
-        {"John Smith", "Buenos Aires", { 1, "Mar", 2022 }},
-        {"Mary Weaver", "Guatemala City", { 10, "Feb", 2022 }},
-        {"James Carpenter", "Santiago de Chile", { 1, "Mar", 2022 }},
-        {"Nancy Gardener", "Guatemala City", { 10, "Mar", 2022 }},
-        {"Elizabeth Clerk", "Quito", { 12, "Apr", 2022 }},
-        {"Thomas Farmhand", "Guatemala City", { 11, "Feb", 2022 }},
-        {"Richard Sailor", "Guatemala City", { 10, "Feb", 2022 }},
+const INVOICE InvoiceList[] = {
+        {"John Smith", "Buenos Aires", {1, "Mar", 2022}},
+        {"Mary Weaver", "Guatemala City", {10, "Feb", 2022}},
+        {"James Carpenter", "Santiago de Chile", {1, "Mar", 2022}},
+        {"Nancy Gardener", "Guatemala City", {10, "Mar", 2022}},
+        {"Elizabeth Clerk", "Quito", {12, "Apr", 2022}},
+        {"Thomas Farmhand", "Guatemala City", {11, "Feb", 2022}},
+        {"Richard Sailor", "Guatemala City", {10, "Feb", 2022}}
 };
 
-char** Exam(const INVOICE** ppInvoices, int nInvoices, const char* pDestination, const char *pDate, int *pnResults)
-{
-    if(!ppInvoices || !pDestination || !pDate || !pnResults)
-        return NULL;
 
-    int i, j=0;
-    char **PassengerList = (char**)malloc(sizeof(char*)*nInvoices);
-    char date[11];
-    sprintf(date, "%d-%s-%d", ppInvoices[0]->when.day, ppInvoices[0]->when.month, ppInvoices[0]->when.year);
+int isDateValid(DATE *date, const char *pDate) {
 
-    for(i=0; i<nInvoices; i++) {
-        if(strcmp(ppInvoices[i]->pDestination, pDestination)==0 && strcmp(date, pDate)==0) {
-            PassengerList[j] = (char*)malloc(sizeof(char)*(strlen(ppInvoices[i]->pPassenger)+1));
-            strcpy(PassengerList[j], ppInvoices[i]->pPassenger);
-            j++;
-        }
-    }
-    *pnResults = j;
-
-    return PassengerList;
-}
-
-int main()
-{
-    int n;
-    char **PassengerList = Exam(InvoiceList, 7, "Guatemala City", "10-Feb-2022", &n);
-    if(!PassengerList) {
-        printf("Invalid input\n");
+    int d, m, y;
+    if (sscanf(pDate, "%d-%3s-%d", &d, date->month, &y) != 3) {
         return 0;
     }
-    printf("Input date *day-month(3letters)-year: \n");
-    scanf("&19s", pDate);
+    date->day = d;
+    date->year = y;
 
-    printf("Input name and destination city: \n");
-    scanf("&19s,&19s", passengerList[], pDestination);
-    int i;
-    printf("List of passengers flying to %s on %s:\n");
-    for(i=0; i<n; i++) {
 
-        printf("%s\n", PassengerList[i]);
+    if (strcmp(date->month, "Jan") && strcmp(date->month, "Feb") && strcmp(date->month, "Mar") &&
+        strcmp(date->month, "Apr") && strcmp(date->month, "May") && strcmp(date->month, "Jun") &&
+        strcmp(date->month, "Jul") && strcmp(date->month, "Aug") && strcmp(date->month, "Sep") &&
+        strcmp(date->month, "Oct") && strcmp(date->month, "Nov") && strcmp(date->month, "Dec")) {
+        return 0;
     }
 
-    for(i=0; i<n; i++) {
-        free(PassengerList[i]);
+
+    if (d < 1 || d > 31 || y < 1900 || y > 2100) {
+        return 0; // Invalid day or year
     }
-    free(PassengerList);
+
+    return 1;
+}
+
+char **TravelAgent(const INVOICE *pInvoices, int nInvoices, const char *pDestination, const char *pDate, int *pnResults) {
+    // Validate input parameters
+    if (pInvoices == NULL || pDestination == NULL || pDate == NULL || nInvoices <= 0) {
+        *pnResults = 0;
+        return NULL;
+    }
+
+
+    char **resultList = (char **)malloc(nInvoices * sizeof(char *));
+    int resultCount = 0;
+
+
+    DATE inputDate;
+    if (!isDateValid(&inputDate, pDate)) {
+        *pnResults = 0;
+        free(resultList);
+        return NULL;
+    }
+
+
+    for (int i = 0; i < nInvoices; i++) {
+        if (strcmp(pInvoices[i].pDestination, pDestination) == 0 &&
+            memcmp(&pInvoices[i].when, &inputDate, sizeof(DATE)) == 0) {
+            resultList[resultCount++] = strdup(pInvoices[i].pPassenger);
+        }
+    }
+
+    *pnResults = resultCount;
+    return resultList;
+}
+
+int main() {
+    int n;
+    char **PassengerList = TravelAgent(InvoiceList, 7, "Buenos Aires", "1-Mar-2022", &n);
+
+    if (PassengerList) {
+        printf("Matching passengers:\n");
+        for (int i = 0; i < n; i++) {
+            printf("%s\n", PassengerList[i]);
+            free(PassengerList[i]);
+        }
+        free(PassengerList);
+    } else {
+        printf("No matching passengers found.\n");
+    }
 
     return 0;
 }
